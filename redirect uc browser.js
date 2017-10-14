a14e01b79a69016897b6a4d3028eff32a9cd9f99
Wawan Setiawan <32796982+wawan301@users.noreply.github.com>
2017-10-14 21:38:49 +0700

    ucbrowser

---
+++ redirect uc browser.js
@@ -0,0 +1,7 @@
+<script type="text/javascript">
+var find = navigator.userAgent;
+var redirect = find.search("UCBrowser");
+if(redirect>1) {
+var OpenChrome = window.location.assign("googlechrome://navigate?url="+ window.location.href);
+var activity = OpenChrome;document.getElementsByTagName('head')[0].appendChild(activity);
+}</script>
